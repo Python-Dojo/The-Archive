667ea7baf9cc7a1eb4cc75efc166c8ede92c8cd5
David Jordan <dave@Dangerbook-Pro.broadband>
2023-11-06 21:05:26 +0000

    Add clarifying comment to Placement class

--- 2023-Oct-18/Dave/placement.py
+++ 2023-Oct-18/Dave/placement.py
@@ -8,6 +8,8 @@ class Placement:
     position: Point
     direction: Direction
 
+    # This allows us to unpack a placement with the syntax:
+    # position, direction = placement
     def __iter__(self):
         yield self.position
         yield self.direction
