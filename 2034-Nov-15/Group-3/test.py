d7f0db406809fbaf65b07650fe6bad7370fba264
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2023-11-23 09:23:47 +0000

    G3 2

---
+++ 2034-Nov-15/Group-3/test.py
@@ -0,0 +1,23 @@
+class SimpleClass:
+    """simple class"""
+
+    def __str__(self) -> str:
+        return "simple class"
+
+class SecondSimpleClass:
+    """class with extra arguments in initialiser"""
+
+    def __str__(self) -> str:
+        return "second simple class"
+
+class Cookie:
+    """cookie"""
+
+    def __str__(self) -> str:
+        return "cookie"
+
+class Bus:
+    """bus"""
+
+    def __init__(self) -> str:
+        return "bus"
